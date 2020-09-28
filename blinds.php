66377065e9adafcc021869b9fe43da931acf33c2
Ike Felix <55805592+Ikennaf1@users.noreply.github.com>
2020-09-28 19:00:53 +0100

    Update blinds.php
    update to v0.0.7

--- blinds.php
+++ blinds.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Blinds
- * @version 0.0.1
+ * @version 0.0.7
  */
 
 /**
@@ -9,7 +9,7 @@
  * Plugin URI: https://ikennaf1.github.io/blinds/
  * Description: A plugin that enables WordPress end users view a blog or website in night or dark mode
  * Author: Ike Felix
- * Version: 0.0.1
+ * Version: 0.0.7
  * Author URI: ikennaf1.github.io
  * License: GPLv2 or later
  */
@@ -25,7 +25,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 function osd_blinds_activation()
 {
-    register_activation_hook(__FILE__, 'osd_blinds_activation');
+    	register_activation_hook(__FILE__, 'osd_blinds_activation');
 }
 
 /**
@@ -34,21 +34,31 @@ function osd_blinds_activation()
  */
 function osd_blinds_deactivation()
 {
-    register_deactivation_hook(__FILE__, 'osd_blinds_deactivation');
-    if (isset($_COOKIE['osd_blinds_cookie'])) {
+    	register_deactivation_hook(__FILE__, 'osd_blinds_deactivation');
+    	if (isset($_COOKIE['osd_blinds_cookie'])) {
         setcookie('osd_blinds_cookie', '', time()-3600);
         unset($_COOKIE['osd_blinds_cookie']);
-    }
+    	}
 }
 
 function osd_blinds_widget($content)
 {
-    $content .= '<div style="position: fixed; top: 3rem; right: 3rem; z-index: 9999999999;" id="osd_blinds_widget_id"></div>';
-    if (is_admin() || is_network_admin()) {
+	global $post;
+    	$content .= '<div style="position: fixed; top: 3rem; right: 3rem; z-index: 9999999999;" id="osd_blinds_widget_id"></div>';
+    	if (is_admin() || is_network_admin()) {
         echo $content;
         return;
     }
-    return $content;
+	
+	if ($post->post_type == 'post-new.php') {
+		return $content;
+	}
+	
+	if ($post->post_type == 'product') {
+		echo $content;
+	}
+	
+	return $content;
 }
 
 /**
@@ -56,16 +66,17 @@ function osd_blinds_widget($content)
  */
 function osd_blinds_filter()
 {
-    add_filter('the_content', 'osd_blinds_widget');
-    add_filter('the_excerpt', 'osd_blinds_widget');
+    	add_filter('the_content', 'osd_blinds_widget');
+    	add_filter('the_excerpt', 'osd_blinds_widget');
+	add_filter('woocommerce_before_shop_loop', 'osd_blinds_widget');
+	add_filter('woocommerce_before_single_product', 'osd_blinds_widget');
 
-    if (is_admin() || is_network_admin()) {
-    add_filter('in_admin_header', 'osd_blinds_widget');
-    return;
+    	if (is_admin() || is_network_admin()) {
+    	add_filter('in_admin_header', 'osd_blinds_widget');
+    	return;
     }
 
-    echo apply_filters('woocommerce_after_single_product', '<div style="position: fixed; top: 3rem; right: 3rem; z-index: 9999999999;" id="osd_blinds_widget_id"></div>');
-    return;
+	return;
 }
 
 //wp_enqueue_script('blinds.js', plugins_url('/blinds/blinds.js'), '', filemtime(dirname(__FILE__, 1).'/blinds.js'));
